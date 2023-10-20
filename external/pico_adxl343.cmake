8a5515de99d4ea5228d06dfd244735d2ad6da376
ranguli <hello@joshmurphy.ca>
2023-10-20 15:10:34 -0230

    Tweak the CMake configuration for library consumers

--- external/pico_adxl343.cmake
+++ external/pico_adxl343.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   adxl343
   GIT_REPOSITORY https://github.com/pcjpl/pico-adxl343 
-  GIT_TAG        main  # Or specific commit, tag, etc.
+  GIT_TAG        main
 )
 
 FetchContent_MakeAvailable(adxl343)
