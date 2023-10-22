ac44cc3c12ae7a99501c6cd2205d74319a38b64d
ranguli <hello@joshmurphy.ca>
2023-10-22 16:03:25 -0230

    Remove external/

--- external/pico_adxl343.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-  adxl343
-  GIT_REPOSITORY https://github.com/pcjpl/pico-adxl343 
-  GIT_TAG        main  # Or specific commit, tag, etc.
-)
-
-FetchContent_MakeAvailable(adxl343)
